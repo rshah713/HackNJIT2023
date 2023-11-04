c09f3bc937ad5d86b71a6383052d32af5a33bcce
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-11-04 15:04:14 -0400

    fix ems bug

--- ems_login.py
+++ ems_login.py
@@ -1,8 +1,14 @@
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.utils import rgba
+
+from colors import Colors
 
 
 kv = Builder.load_file('ems_login.kv')
 class EMSLogin(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(EMSLogin, self).__init__(**kwargs)
+        Window.clearcolor = rgba(*Colors.BEIGE, 1)
