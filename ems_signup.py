cf9db1906d296a7ac961b54c5701ccd0ea795422
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-11-04 17:09:56 -0400

    get data from signup screen

--- ems_signup.py
+++ ems_signup.py
@@ -14,3 +14,9 @@ class EMSSignup(Screen):
     
     def on_pre_leave(self, *args):
         self.manager.transition.direction = 'down'
+        
+    def valid_credentials(self, email, pw, pw_confirm, ems_id):
+        if pw.text != pw_confirm.text:
+            return False
+        print(email.text, pw.text, pw_confirm.text, ems_id.text)
+        return True
