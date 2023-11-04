8812a8d28394ceb89a28dc3a8027a1573c41fb31
Brett Dorfman <123748945+Bdorf04@users.noreply.github.com>
2023-11-04 12:10:41 -0400

    Debugged Rohans Mistakes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager
 
 from welcome_window import Welcome
-
+from ems_login import EMSLogin
 class WindowManager(ScreenManager):
     pass
 
