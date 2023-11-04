5d891d8ac1ca803d2658f2d6bda6554da1038171
Brett Dorfman <123748945+Bdorf04@users.noreply.github.com>
2023-11-04 15:24:08 -0400

    Copied gui over to patient login

--- patient_login.py
+++ patient_login.py
@@ -1,8 +1,14 @@
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.utils import rgba
+
+from colors import Colors
 
 
 kv = Builder.load_file('patient_login.kv')
 class PatientLogin(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(PatientLogin, self).__init__(**kwargs)
+        Window.clearcolor = rgba(*Colors.BEIGE, 1)
--- welcome_window.py
+++ welcome_window.py
@@ -1,7 +1,14 @@
-from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.utils import rgba
+
+from colors import Colors
 
 kv = Builder.load_file("welcome.kv")
                        
 class Welcome(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(Welcome, self).__init__(**kwargs)
+        Window.clearcolor = rgba(*Colors.BEIGE, 1)
