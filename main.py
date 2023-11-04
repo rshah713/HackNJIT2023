5fccceb0cc0f4f58a3d6441e133c639e37d2d82a
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-11-04 11:45:09 -0400

    create project structure

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager
 
-class MyApp(App):
+from welcome_window import Welcome
+
+class WindowManager(ScreenManager):
+    pass
+
+class MedifyApp(App):
     def build(self):
         return Label(text='Hello, Kivy!')
 
 if __name__ == '__main__':
-    MyApp().run()
+    MedifyApp().run()
