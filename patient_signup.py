f2a4469cfa9b8904dda36ce923d2c4cc06528c3c
Brett Dorfman <123748945+Bdorf04@users.noreply.github.com>
2023-11-04 15:57:29 -0400

    testing pateint create acc

---
+++ patient_signup.py
@@ -0,0 +1,19 @@
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.utils import rgba
+
+from colors import Colors
+
+
+kv = Builder.load_file('patient_login.kv')
+class PatientSignup(Screen):
+    def __init__(self, **kwargs):
+        super(PatientSignup, self).__init__(**kwargs)
+        Window.clearcolor = rgba(*Colors.BEIGE, 1)
+
+    def get_info(self, patpass, patemail):
+        print(patemail.text)
+        print(patpass.text)
+        
