bc99e5a82b4c0d123d20f6c3a5828ccbd364ac2b
Brett Dorfman <123748945+Bdorf04@users.noreply.github.com>
2023-11-05 00:31:17 -0400

    Credentials can now be sent to db

--- dashboard.py
+++ dashboard.py
@@ -7,8 +7,21 @@ from kivy.utils import rgba
 from colors import Colors
 
 kv = Builder.load_file("dashboard.kv")
-                       
+from FirebaseRealtimeDB import update_patient_info                
 class Dashboard(Screen):
     def __init__(self, **kwargs):
         super(Dashboard, self).__init__(**kwargs)
         Window.clearcolor = rgba(*Colors.BEIGE, 1)
+
+    def update_creds(self, info):
+        info=[i.text for i in info]
+        infodic={"name": info[0],
+                 "age": info[1],
+                 "weight": info[2],
+                 "height": info[3],
+                 "blood type": info[4],
+                 "dlid": info[5],
+                 "medical conditions": info[6],
+                 "allergies": info[7],
+                 "medication": info[8],}
+        update_patient_info(infodic)
