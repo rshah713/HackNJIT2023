9e4a30f199f29f41c020d9a67e75c92a38d53571
jonastirona <jonastirona1@gmail.com>
2023-11-04 21:54:14 -0400

    add more verification

--- ems_login.py
+++ ems_login.py
@@ -4,7 +4,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.utils import rgba
 
-from FirebaseRealtimeDB import login as fb_login
+from FirebaseRealtimeDB import login as fb_login, verify_emt
 from colors import Colors
 
 
@@ -15,9 +15,11 @@ class EMSLogin(Screen):
         Window.clearcolor = rgba(*Colors.BEIGE, 1)
     
     def login(self, email, password, ems_id, errorlbl):
+        errorlbl.text = ''
         print(email, password)
-        if len(ems_id.text) < 12:
+        if not verify_emt(ems_id.text):
             errorlbl.text = 'National EMS ID could not be verified'
             ems_id.text = ''
             return False
+        print(fb_login(email, password))
         return fb_login(email, password)
