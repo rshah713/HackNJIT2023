d5c9709f28319a605dde4c0af697b409035fcce8
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-11-04 22:41:49 -0400

    add popup

--- ems_dinput.py
+++ ems_dinput.py
@@ -3,6 +3,9 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.utils import rgba
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 from colors import Colors
 from FirebaseRealtimeDB import verify_emt
@@ -10,5 +13,14 @@ from FirebaseRealtimeDB import verify_emt
 
 kv = Builder.load_file('ems_dinput.kv')
 class EMSDinput(Screen):
+    
     def check_dl_num(self, license):
-        return verify_emt(license)
+        b = Button(text="Error verifying license")
+        is_valid = verify_emt(license)
+        if not is_valid:
+            popup = Popup(title='Test popup',
+    content=b, size=(200, 400))
+            b.bind(on_press=popup.dismiss)
+            popup.open()
+            return False
+        return True
